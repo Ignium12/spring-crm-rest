a93c32ca4fe0b2c5d913b7b745648620e458053a
Ignium12 <kevin.ruehl12@googlemail.com>
2021-02-01 17:37:53 +0100

    Added DeleteMapping for deleting a customer from the database

--- src/main/java/com/kruehl/spring/rest/CustomerRestController.java
+++ src/main/java/com/kruehl/spring/rest/CustomerRestController.java
@@ -49,4 +49,17 @@ public class CustomerRestController {
         return customer;
     }
 
+    @DeleteMapping("/customers/{customerId}")
+    public String deleteCustomer(@PathVariable int customerId){
+
+        Customer tempCustomer = customerService.getCustomer(customerId);
+        if(tempCustomer == null){
+            throw new CustomerNotFoundException("Customer id not found - " + customerId);
+        }
+
+        customerService.deleteCustomer(customerId);
+
+        return "Deleted customer id - " + customerId;
+    }
+
 }
